4352cf321effedbf31049287b1ba83e849c20a59
Ashwin Nikam <anikam@buffalo.edu>
2017-08-01 20:54:39 -0400

    Added listeners for radiobuttons

--- app/src/main/java/com/example/android/ticker/NewTaskActivity.java
+++ app/src/main/java/com/example/android/ticker/NewTaskActivity.java
@@ -2,23 +2,60 @@ package com.example.android.ticker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class NewTaskActivity extends AppCompatActivity {
 
-    public Button mCreateTaskButton;
+    public static RadioGroup mRadioGroup;
+    public static EditText mTaskDescription;
+    public static Button mCreateTaskButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
 
+        mRadioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        mTaskDescription = (EditText) findViewById(R.id.et_enter_task);
+
         mCreateTaskButton = (Button) findViewById(R.id.button_save);
+        mCreateTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createTask();
+            }
+        });
     }
 
-    public void create_task(View view) {
-        Toast.makeText(this, "Not implemented yet", Toast.LENGTH_SHORT).show();
+    public static void createTask() {
+        String taskDescription = String.valueOf(mTaskDescription.getText());
+        mTaskDescription.setText("");
+
+        switch (mRadioGroup.getCheckedRadioButtonId()) {
+            case R.id.rb_high:
+                Log.i("RB", "High");
+                break;
+
+            case R.id.rb_medium:
+                Log.i("RB", "Medium");
+                break;
+
+            case R.id.rb_low:
+                Log.i("RB", "Low");
+                break;
+
+            default:
+                throw new IllegalArgumentException("Radiobutton not valid");
+        }
+        mRadioGroup.clearCheck();
+        Log.i("Task", taskDescription);
     }
+
 }
