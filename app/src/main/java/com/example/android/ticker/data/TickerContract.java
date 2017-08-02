4f40736c89dc625f4df1f0b8ce04e4a967610f0c
Ashwin Nikam <anikam@buffalo.edu>
2017-08-01 20:27:51 -0400

    TaskEntry class implemented

---
+++ app/src/main/java/com/example/android/ticker/data/TickerContract.java
@@ -0,0 +1,33 @@
+package com.example.android.ticker.data;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by ashwin on 8/1/17.
+ */
+
+public class TickerContract {
+
+    public static final String CONTENT_AUTHORITY = "com.example.android.ticker";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://"+CONTENT_AUTHORITY);
+    public static final String PATH_TASKS = "task";
+
+    public static final class TickerEntry implements BaseColumns {
+
+        //First we created the main content uri
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI
+                .buildUpon()
+                .appendPath(PATH_TASKS)
+                .build();
+
+        //Then we declared the table name, and name of all the columns
+
+        public static final String TABLE_NAME = "task";
+        public static final String COLUMN_TASK_NAME = "task_name";
+        public static final String COLUMN_PRIORITY = "priority";
+
+    }
+
+}
